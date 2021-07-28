00b161a189292ea6ef0257d81b7eba46f997e7b6
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-07-28 15:47:48 -0300

    add posts page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return(
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a monorepo with lerna and yarn workspaces</strong>
+						<p>In this guide, you will learn how to create a monorepo to manage multiple packages</p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a monorepo with lerna and yarn workspaces</strong>
+						<p>In this guide, you will learn how to create a monorepo to manage multiple packages</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
